d1ccd929b93b0f0571b2d1578baa4114b14ad3b5
Mikhail <misha-melnik-03@mail.ru>
2022-11-04 10:34:41 -0400

    Chapter1 - Added Ammunition Manager

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -7,6 +7,10 @@ public class Gun : MonoBehaviour
     public float fireRate;
     protected float lastFireTime;
 
+    public Ammo ammo;
+    public AudioClip liveFire;
+    public AudioClip dryFire;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +23,15 @@ public class Gun : MonoBehaviour
     }
     protected void Fire()
     {
+        if (ammo.HasAmmo(tag))
+        {
+            GetComponent<AudioSource>().PlayOneShot(liveFire);
+            ammo.ConsumeAmmo(tag);
+        }
+        else
+        {
+            GetComponent<AudioSource>().PlayOneShot(dryFire);
+        }
         GetComponentInChildren<Animator>().Play("Fire");
     }
 }
