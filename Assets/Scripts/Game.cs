3d070dbf141f6969c5c7d6bfe868280ab5687a06
Mikhail <misha-melnik-03@mail.ru>
2022-11-13 22:44:05 -0500

    Chapter 10 - Added Spawn Points for Robots to Scene

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private static Game singleton;
+    [SerializeField]
+    RobotSpawn[] spawns;
+    public int enemiesLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        singleton = this;
+        SpawnRobots();
+    }
+    // 2
+    private void SpawnRobots()
+    {
+        foreach (RobotSpawn spawn in spawns)
+        {
+            spawn.SpawnRobot();
+            enemiesLeft++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
