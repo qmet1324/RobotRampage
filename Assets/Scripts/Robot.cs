1e3703a1cc72644080bf8762703844ca483d537d
Mikhail <misha-melnik-03@mail.ru>
2022-11-13 21:18:26 -0500

    Chapter 10 - Added Robot Fire Animation

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -15,6 +15,8 @@ public class Robot : MonoBehaviour
     private float timeLastFired;
     private bool isDead;
 
+    public Animator robot;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -46,6 +48,6 @@ public class Robot : MonoBehaviour
     }
     private void fire()
     {
-        Debug.Log("Fire");
+        robot.Play("Fire");
     }
 }
